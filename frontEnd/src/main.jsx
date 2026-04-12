cfb920d8b0b66c85d9d13bcc0d26369e2d2d445f
Tony Shangkuan @ Desktop <tonyshangkuan@gmail.com>
2026-04-12 23:18:49 +0800

    add custom.scss to customize bootstrap primary color

--- frontEnd/src/main.jsx
+++ frontEnd/src/main.jsx
@@ -1,5 +1,13 @@
 import "./index.css"; // 引入全局 CSS 樣式
 
+import "bootstrap/dist/css/bootstrap.min.css"; // 引入 Bootstrap 的 CSS；
+// 注意：這裡引入的是「整個 Bootstrap 的 CSS」，而不是「Bootstrap 的 React 組件」
+import "bootstrap/dist/js/bootstrap.bundle.min.js"; // 引入 Bootstrap 的 JavaScript；
+// 注意：這裡引入的是「整個 Bootstrap 的 JavaScript」，而不是「Bootstrap 的 React 組件」
+// 這樣做的目的是為了讓 Bootstrap 的 JavaScript 功能（例如：下拉選單、模態框等）能夠正常運作
+
+import "./custom.scss"; // 引入 custom.scss；這個檔案裡面有「改變 Bootstrap 預設樣式的 SCSS 變數」
+
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
