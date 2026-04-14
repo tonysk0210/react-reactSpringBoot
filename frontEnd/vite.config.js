61bc0d2142e0b57de8cbdd8f376cec1e5c22abb0
Tony Shangkuan @ Desktop <tonyshangkuan@gmail.com>
2026-04-15 00:47:01 +0800

    add Tailwind

--- frontEnd/vite.config.js
+++ frontEnd/vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite"; // 引入 Tailwind CSS 的 Vite 插件
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()], // tailwindcss () 使用 Tailwind CSS 的 Vite 插件
   // 設定開發伺服器的port為3000
   // server: {
   //   port: 3000,
