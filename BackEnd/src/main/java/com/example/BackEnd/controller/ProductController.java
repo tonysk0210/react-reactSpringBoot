4a7d6a7a3ae9fb21dccf7a379a5001a2eb71c759
Tony Shangkuan @ Desktop <tonyshangkuan@gmail.com>
2026-04-17 06:19:00 +0800

    add h2db

---
+++ BackEnd/src/main/java/com/example/BackEnd/controller/ProductController.java
@@ -0,0 +1,15 @@
+package com.example.BackEnd.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/products")
+public class ProductController {
+
+    @GetMapping
+    public String getProducts() {
+        return "Here are products";
+    }
+}
