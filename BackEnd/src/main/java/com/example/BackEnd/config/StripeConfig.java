1276bb54507823d3bc052a0a8e9dbe4214b79681
E3378 <anthony.hnsquare@gmail.com>
2026-05-20 13:04:55 +0800

    add stripe.properties

--- BackEnd/src/main/java/com/example/BackEnd/config/StripeConfig.java
+++ BackEnd/src/main/java/com/example/BackEnd/config/StripeConfig.java
@@ -4,8 +4,10 @@ import com.stripe.Stripe;
 import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration
+@PropertySource("classpath:stripe.properties") // 指定 properties 檔案路徑
 public class StripeConfig {
 
     @Value("${stripe.apiKey}") // 從application.properties檔案中取得 Stripe API 金鑰
